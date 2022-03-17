764c4c723617411502722ef87c41f67fb6e857ce
Ahmad Rifa'i <arifai209@gmail.com>
2022-03-17 13:12:46 +0700

    [ADD] location permission

--- lib/ui/screen/map/map_page.dart
+++ lib/ui/screen/map/map_page.dart
@@ -2,8 +2,10 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:klinik/core/core.dart';
+import 'package:location/location.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -15,11 +17,28 @@ class MapPage extends StatefulWidget {
 class _MapPageState extends State<MapPage> {
   final Completer<GoogleMapController> _controller = Completer();
   final List<Marker> _markers = <Marker>[];
+  final Location _location = Location();
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(-7.3228857, 109.8966993),
-    zoom: 12.5,
+    zoom: 11.0,
   );
+
+  _getCurrentLocation() async {
+    try {
+      await _location.getLocation();
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        print('PERMISSION_DENIED');
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    _getCurrentLocation();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +51,7 @@ class _MapPageState extends State<MapPage> {
     _markers.add(
       Marker(
         markerId: MarkerId("Position"),
-        position: LatLng(-7.3228857, 109.8966993),
+        position: LatLng(-8.0997773, 112.0046038),
       ),
     );
 
@@ -40,6 +59,7 @@ class _MapPageState extends State<MapPage> {
       width: Core.getDefaultAppWidth(context),
       height: Core.getDefaultBodyHeight(context),
       child: GoogleMap(
+        myLocationEnabled: true,
         onMapCreated: _onMapCreated,
         initialCameraPosition: _kGooglePlex,
         markers: Set<Marker>.of(_markers),
